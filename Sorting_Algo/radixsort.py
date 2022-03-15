f015a2300ada8ac2deb8a4d7ae8b55d3c0573a2a
jahnavim111 <96688209+jahnavim111@users.noreply.github.com>
2022-03-15 19:26:43 +0530

    Update radixsort.py

--- Sorting_Algo/radixsort.py
+++ Sorting_Algo/radixsort.py
@@ -1,31 +1,24 @@
-#Radix Sort
+#Radix Sorting
 
-def radixsort(a):
-	bucket=[[]]*10
-	b=[]
-	#for number of iterations
-	div=1	
-	tmp=[]
-	for i in range(len(str(max(a)))):
-		for j in range(len(a)):
-			digit=(a[j]//div)%10
-			if(len(bucket[digit])==0):
-				bucket[digit]=a[j]
-			else:
-				bucket[digit].append(a[j])
-		for i in range(10):
-			tmp.extend(bucket[i])
-		div=div*10
-		
-				
+def radix_sort(arr):
+    for k in range(1,len(str(max(arr)))+1):
+        buckets = [[]]*10
+        for i in arr:
+            c=(i%(10**k))//(10**(k-1))
+            if len(buckets[c])==0:
+                buckets[c]=[i]
+            else:
+                buckets[c].append(i)
+            tmp = []
+        for i in range(10):
+            tmp.extend(buckets[i])
+        
+        arr = tmp
 
-a=[]
-for _ in range(int(input())):
+    return arr
+arr=[]
+for i in range(int(input())):
 	x=int(input())
-	a.append(x)
-		
-radixsort(a)
-print("After Sorting",a)
-	
-	
+	arr.append(x)
+print("\nThe sorted array is:",radix_sort(arr))
 	
